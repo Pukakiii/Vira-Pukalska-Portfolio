6970fcc47b6a270e1140f71a85960d519880d734
Pukakiii <pukaki.ip@gmail.com>
2026-07-02 03:21:01 +0200

    feat: add about and credits sections

---
+++ components/credits.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Credits = (props) => {
+ return (
+ <div data-section id="credits" className="group mt-32">
+ <div className="text-surface-600">
+ <div className="flex flex-row justify-between">
+ <a
+ href="/imprint"
+ rel="noopener noreferrer"
+ className="scroll-auto transition-all hover:text-on-background"
+ >
+ Imprint
+ </a>
+ <a
+ href="/privacy"
+ rel="noopener noreferrer"
+ className="scroll-auto transition-all hover:text-on-background"
+ >
+ Privacy Statement
+ </a>
+ <span>
+ {props.data.name} | {new Date().getFullYear()}
+ </span>
+ </div>
+ </div>
+ </div>
+ );
+};
+
+export default Credits;
